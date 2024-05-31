0a9f29ec7b89553ed8c5f35d479d2cf7e85e1127
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2024-05-31 16:46:52 +0500

    java

---
+++ js/index.js
@@ -0,0 +1,145 @@
+// 1-masala
+// let a = +prompt("kvadrt-tomoninikiriting");
+// let p = 4 * a;
+// console.log(p);
+// 2 - masala;
+// let a = +prompt("kvadrt-yumoninikiriting");
+// let S = a * a;
+// console.log(S);
+// 3 - masala;
+// let a = +prompt("tortburchakning a -tomonini kiriting");
+// let b = +prompt("tortburchakning b -tomonini kiriting");
+// let S = a * b;
+// let p = 2*(a + b);
+// console.log(S);
+// console.log(p);
+// 4-masla
+// let d = +prompt("aylananing diametrini kiriting");
+// let L=Math.PI*d;
+// console.log(L);
+// 5-massla
+// let a = +prompt("kubning-tomoninikiriting");
+// let V = a * a * a;
+// console.log(V);
+// 6 - masala;
+// let a = +prompt("paralelepipedning-tomoninikiriting");
+// let b = +prompt("paralelepipedning-tomoninikiriting");
+// let c = +prompt("paralelepipedning-tomoninikiriting");
+// let V = a * b * c;
+// let S = 2 * (a * b + a * c + b * c);
+// console.log(V);
+// console.log(S);
+// 7-masala
+// let R = +prompt("doiraning radiusini kiriting");
+// let L=2*Math.PI*R;
+// let S=Math.PI*R*R;
+// console.log(L);
+// console.log(S);
+// 8-masala
+// let a = +prompt('a sonni kiriting');
+// let b = +prompt("b sonni kiriting");
+// let o = a + b / 2;
+// console.log(o);
+// 9-masala
+// let R1=+prompt("1aylananing radiusini kiriting");
+// let R2=+prompt('2aylananing radiusini kiriting');
+// if(R1>R2){
+//     let S1=math.PI*R1
+//     let S2=math.PI*R2
+//     let S3=math.PI(R1-R2);
+//     console.log(S1);
+//     console.log(S2);
+//     console.log(S3);
+// }
+// 10-masala
+// let Tf = +prompt("Tf gradusni kiriting");
+// let Tc = (Tf - 32) * 5 / 9;
+// console.log(Tc);
+
+// Keyinfi vazifa
+// 1-masala
+// let uzunlikSm = +prompt("L sm sonni kiriting");
+// let uzunlikM = uzunlikSm / 100;
+// console.log(uzunlikM);
+// 2-masala
+// let kilogram = +prompt("kilogram sonni kiriting");
+// let tonna = kilogram / 1000;
+// console.log(tonna);
+// 3-masala
+// let bayt=+prompt ('baytni kritinfing');
+// let kilobayt=bayt/1024;
+// console.log(kilobayt);
+// 4-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// if (a > b) {
+//   let joylashishi = Math.floor(a / b);
+//   console.log("a kesmada b kesmaning joylashgan qismi"+joylashganqismi);
+// } else {
+//   console.log("a qiymati b qiymatidan katta bo'lishi kerak.");
+// }
+// 5-masala
+//  let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// if (a > b) {
+//   let joylashmagan_qismi = a%b;
+// console.log( "a kesmada b kesmaning joylashmagan qismi"+joylashmagan_qismi);
+// } else {
+//   console.log("a qiymati b qiymatidan katta bo'lishi kerak.");
+// }
+// 6-masala
+// let a = +prompt(" 2 xonali sonni kiriting");
+// let onlar_xonasidagiraqami=Math.floor(a/10);
+// let birlar_xonasidagiraqami=a%10;
+// console.log(onlar_xonasidagiraqami);
+// console.log(birlar_xonasidagiraqami);
+// 7 - masala;
+// let a = +prompt("2 xonali sonni kiriting");
+// let onlar_xonasidagiraqami = Math.floor(a / 10);
+// let birlar_xonasidagiraqami = a % 10;
+// let yigindi = onlar_xonasidagiraqami + birlar_xonasidagiraqami;
+// console.log(yigindi);
+// 8-masala
+// let a = +prompt("2 xonali sonni kiriting");
+// let onlar_xonasidagiraqami = Math.floor(a / 10);
+// let birlar_xonasidagiraqami = a % 10;
+// let yigindi = birlar_xonasidagiraqami * 10 + onlar_xonasidagiraqami;
+// console.log("sonning teskarisi" + yigindi);
+// 9-masala
+// let a = +prompt("3 xonali sonni kiriting");
+// let yuzlar_xonasidagiraqami = Math.floor(a / 100);
+// console.log("100lar xonasidagi raqam: " + yuzlar_xonasidagiraqami);
+// 10 - masala;
+// let a = +prompt("3 xonali sonni kiriting");
+// let birlar_xonasidagiraqami = a % 10;
+// let onlar_xonasidagiraqami = Math.floor((a % 100) / 10);
+// console.log(birlar_xonasidagiraqami);
+// console.log(onlar_xonasidagiraqami);
+// 11-masala
+// let a = +prompt("3 xonali sonni kiriting");
+// let birlar_xonasidagiraqami = a % 10;
+// let onlar_xonasidagiraqami = Math.floor((a % 100) / 10);
+// let yuzlar_xonasidagiraqami = Math.floor(a / 100);
+// console.log(
+//   birlar_xonasidagiraqami + onlar_xonasidagiraqami + yuzlar_xonasidagiraqami
+// );
+// 12-masala
+// let a = +prompt("3 xonali sonni kiriting");
+// let birlar_xonasidagiraqami = a % 10;
+// let onlar_xonasidagiraqami = Math.floor((a % 100) / 10);
+// let yuzlar_xonasidagiraqami = Math.floor(a / 100);
+// console.log(
+//   birlar_xonasidagiraqami * 100 +
+//     onlar_xonasidagiraqami * 10 +
+//     yuzlar_xonasidagiraqami
+// );
+// 13-masala
+// let a = +prompt("3 xonali sonni kiriting");
+// let birlar_xonasidagiraqami = a % 10;
+// let onlar_xonasidagiraqami = Math.floor((a % 100) / 10);
+// let yuzlar_xonasidagiraqami = Math.floor(a / 100);
+// console.log(
+//   onlar_xonasidagiraqami * 100 +
+//     birlar_xonasidagiraqami * 10 +
+//     yuzlar_xonasidagiraqami
+// );
